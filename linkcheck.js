e0ad85bf2c68931161b430a392cba6abfd6f3dc0
Serenity62 <Serenity62@users.noreply.github.com>
2025-04-26 11:40:44 -0400

    Identify which link shorter is being used.

--- linkcheck.js
+++ linkcheck.js
@@ -1,20 +1,52 @@
+const short_urls = [
+	"bit.ly",
+	"shorturl.at",
+	"tinyurl.com",
+	"rb.gy",
+	"ow.ly",
+	"d.to",
+	"b.link",
+	"shortio.li",
+];
+
 function isShortened(url) {
-	// TODO
-	return true;
+	const ans = short_urls.filter((u) => url.includes(u));
+	return ans.length > 0 ? ans[0] : "";
 }
 
 function unShorten(url) {
-	// TODO
+	const newUrl = isShortened(url);
+	switch (newUrl) {
+		case "bit.ly":
+		case "shorturl.at":
+		case "tinyurl.com":
+		case "rb.gy":
+		case "ow.ly":
+		case "d.to":
+		case "b.link":
+		case "shortio.li":
+			return newUrl;
+	}
 	return url;
 }
 
-function handleTabCreate(tab) {
-	const url = tab.url;
-	if (isShortened(url)) {
-		console.log(url);
-		const newUrl = unShorten(url);
-		console.log(newUrl);
+//function handleTabCreate(tab) {
+function handleBeforeNavigate(details) {
+	const url = details.url;
+	const newUrl = unShorten(url);
+	if (url !== newUrl) {
+		console.log(`${url} -> ${newUrl}`);
 	}
 }
 
-browser.tabs.onCreated.addListener(handleTabCreate);
+const filter = {
+	url: short_urls.map((v) => {
+		return { hostContains: v };
+	}),
+};
+
+// browser.tabs.onCreated.addListener(handleTabCreate);
+browser.webNavigation.onBeforeNavigate.addListener(
+	handleBeforeNavigate,
+	filter,
+);
